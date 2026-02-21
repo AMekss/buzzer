024861b140864ef8f371668b2bbaeee77b4413ef
Arturs Mekss <arturs.mekss@gmail.com>
2026-02-21 23:37:19 +0200

    hi bob

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -9,8 +9,7 @@ class GraphqlController < ApplicationController
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
+      current_user: current_user
     }
     result = BuzzerSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
@@ -20,6 +19,11 @@ class GraphqlController < ApplicationController
   end
 
   private
+
+  # IRL would be set from the auth token
+  def current_user
+    @current_user ||= Participant.find_by!(full_name: "Bob")
+  end
 
   # Handle variables in form data, JSON body, or a blank value
   def prepare_variables(variables_param)
